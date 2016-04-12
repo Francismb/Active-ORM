f5937e78035704efbbbf2cb3f823be162d0fb2b5
Francis <frazboyz101@gmail.com>
2016-04-12 20:45:14 +1200

    Active Records now get mapped and created a custom exception

---
+++ src/org/jactiverecord/exceptions/AnnotationRequiredException.java
@@ -0,0 +1,11 @@
+package org.jactiverecord.exceptions;
+
+/**
+ * Created by Francis on 12/04/16.
+ * Project Jactive-Record.
+ */
+public class AnnotationRequiredException extends RuntimeException {
+    public AnnotationRequiredException(final String message) {
+        super(message);
+    }
+}
--- src/org/jactiverecord/mapping/ObjectMapping.java
+++ src/org/jactiverecord/mapping/ObjectMapping.java
@@ -1,6 +1,9 @@
 package org.jactiverecord.mapping;
 
+import org.jactiverecord.exceptions.AnnotationRequiredException;
 import org.jactiverecord.mapping.annotations.Column;
+import org.jactiverecord.mapping.annotations.PrimaryKey;
+import org.jactiverecord.mapping.annotations.Relationship;
 import org.jactiverecord.mapping.annotations.Table;
 
 import java.lang.reflect.Field;
@@ -28,12 +31,35 @@ public class ObjectMapping {
 
     /**
      * Constructs a new {@link ObjectMapping}
+     * and maps the object
      */
     public ObjectMapping() {
-        this.table = getClass().getAnnotation(Table.class);
+        // Set the table annotation
+        if (getClass().isAnnotationPresent(Table.class)) {
+            this.table = getClass().getAnnotation(Table.class);
+        } else {
+            throw new AnnotationRequiredException("Table annotation is required on ActiveRecord");
+        }
         for (final Field field : getClass().getFields()) {
             if (field.isAnnotationPresent(Column.class)) {
-                mappings.add(new FieldMapping(getClass(), field, this));
+                // Create a mapping
+                final FieldMapping mapping = new FieldMapping(getClass(), field, this);
+
+                // Set the mappings column annotations
+                mapping.column = field.getAnnotation(Column.class);
+
+                // Set the primary key annotation if it is present
+                if (field.isAnnotationPresent(PrimaryKey.class)) {
+                    mapping.primaryKey = field.getAnnotation(PrimaryKey.class);
+                }
+
+                // Set the relationship annotation if it is present
+                if (field.isAnnotationPresent(Relationship.class)) {
+                    mapping.relationship = field.getAnnotation(Relationship.class);
+                }
+
+                // Add the mapping
+                mappings.add(mapping);
             }
         }
     }
